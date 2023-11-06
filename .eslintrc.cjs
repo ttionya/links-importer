7815d56db0d6eaa7880b79257cb7eac8fa28fc39
ttionya <git@ttionya.com>
2023-11-06 23:03:44 +0800

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@ttionya/eslint-config'],
+}
---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+export default {
+  '*.js': ['prettier --write', 'eslint --fix'],
+  '*.json': ['prettier --write'],
+}
